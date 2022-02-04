99346ade37b8531739a22d02192410dccb1640db
David Soria Parra <dsp@experimentalworks.net>
2022-02-04 22:41:04 +0000

    rfesi: add simple search

--- src/groups/search.rs
+++ src/groups/search.rs
@@ -2,7 +2,30 @@
 
 use crate::prelude::*;
 
+#[derive(Debug, Deserialize)]
+#[allow(missing_docs)]
+pub struct SearchResult {
+    character: Option<Vec<u32>>,
+    constellation: Option<Vec<u32>>,
+    corporation: Option<Vec<u32>>,
+    solar_system: Option<Vec<u32>>,
+    station: Option<Vec<u32>>,
+}
+
 /// Endpoints for Search
 pub struct SearchGroup<'a> {
     pub(crate) esi: &'a Esi,
+}
+
+impl<'a> SearchGroup<'a> {
+    api_get!(
+        /// Get a character's public information.
+        get_search,
+        "get_search",
+        RequestType::Public,
+        SearchResult,;
+        (categories: &str) => "categories",
+        (search: &str) => "search",
+        (strict: bool) => "strict"
+    );
 }
